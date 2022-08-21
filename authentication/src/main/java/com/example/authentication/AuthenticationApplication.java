fc91d3d2d5826b6182d5ae4b956eee15006eee12
William Makau <williammakau070@gmail.com>
2022-08-21 23:00:01 +0300

    splitting into microservices

--- authentication/src/main/java/com/example/authentication/AuthenticationApplication.java
+++ authentication/src/main/java/com/example/authentication/AuthenticationApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableZuulProxy
 public class AuthenticationApplication {
 
     public static void main(String[] args) {
